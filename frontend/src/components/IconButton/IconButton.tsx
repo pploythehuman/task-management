20888b5977902fdafa5ecb7ae82057c7f2051fea
Kanokpon Nagavajara <kanokpon26@gmail.com>
2024-09-19 19:35:00 +0700

    feat: update name with ux error handling

--- frontend/src/components/IconButton/IconButton.tsx
+++ frontend/src/components/IconButton/IconButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import React from "react";
 import "./IconButton.css";
 
 interface IconButtonProps {
@@ -10,26 +11,28 @@ interface IconButtonProps {
   className?: string;
 }
 
-export default function IconButton({
-  icon,
-  type = "button",
-  onClick,
-  className,
-}: IconButtonProps) {
-  return (
-    <button
-      type={type}
-      onClick={onClick}
-      className={className ? className : "iconButton"}
-    >
-      <div style={{ width: "100%", height: "100%", position: "relative" }}>
-        <Image
-          src={icon}
-          layout="fill"
-          objectFit="contain"
-          alt="Right Arrow Icon"
-        />
-      </div>
-    </button>
-  );
-}
+const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(
+  ({ icon, type = "button", onClick, className }, ref) => {
+    return (
+      <button
+        ref={ref}
+        type={type}
+        onClick={onClick}
+        className={className ? className : "iconButton"}
+      >
+        <div style={{ width: "100%", height: "100%", position: "relative" }}>
+          <Image
+            src={icon}
+            layout="fill"
+            objectFit="contain"
+            alt="icon"
+          />
+        </div>
+      </button>
+    );
+  }
+);
+
+IconButton.displayName = "IconButton";
+
+export default IconButton;
--- frontend/src/components/TaskItem/TaskItem.tsx
+++ frontend/src/components/TaskItem/TaskItem.tsx
@@ -15,6 +15,8 @@ interface TaskItemProps {
 
 export default function TaskItem({ task, onUpdateTask }: TaskItemProps) {
   const inputRef = useRef<HTMLInputElement>(null);
+  const editSaveButtonRef = useRef<HTMLButtonElement>(null);
+  const [name, setName] = useState(task.name);
   const [isComplete, setIsComplete] = useState(task.complete);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -23,6 +25,40 @@ export default function TaskItem({ task, onUpdateTask }: TaskItemProps) {
     setIsComplete(value);
     onUpdateTask(task.id, { complete: value });
   };
+
+  const handleEditSaveToggle = () => {
+    setIsEditing(!isEditing);
+    if (isEditing) {
+      onUpdateTask(task.id, { name });
+    }
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    console.log("editSaveButtonRef", editSaveButtonRef);
+    if (
+      isEditing &&
+      inputRef.current &&
+      !inputRef.current.contains(event.target as Node) &&
+      editSaveButtonRef.current &&
+      !editSaveButtonRef.current.contains(event.target as Node)
+    ) {
+      alert("You haven't finished editing!");
+      setIsEditing(false);
+    }
+  };
+
+  // Alert when have not finshed editing
+  useEffect(() => {
+    if (isEditing) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isEditing]);
 
   // Locate cursor at input when edit
   useEffect(() => {
@@ -34,21 +70,23 @@ export default function TaskItem({ task, onUpdateTask }: TaskItemProps) {
   return (
     <div className="taskItemContainer">
       <TaskItemButton value={isComplete} onClick={handleCompleteToggle} />
+
       <input
         ref={inputRef}
-        value={task.name}
-        className="inputField"
         type="text"
+        defaultValue={name}
         onChange={(event) => {
-          console.log(event.target.value);
+          setName(event.target.value);
         }}
         readOnly={!isEditing}
+        className="inputField"
         style={{ textDecoration: isComplete ? "line-through" : "none" }}
       />
 
       <IconButton
+        ref={editSaveButtonRef}
         icon={isEditing ? checkMarkIcon : pencilIcon}
-        onClick={() => setIsEditing(!isEditing)}
+        onClick={handleEditSaveToggle}
       />
     </div>
   );
